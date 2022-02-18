0a27abb60ab423956b9ca0f2f15b22d2481cc889
Tasneem Ahmad <tasneemahmad@Tasneems-PC.local>
2022-02-18 14:14:06 +0800

    initial

---
+++ springhelloweb/src/main/java/com/ncs/controller/advice/TodoControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.ncs.controller.advice;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ncs.exception.ErrorResponse;
+import com.ncs.exception.TodoNotFoundException;
+
+@RestControllerAdvice
+public class TodoControllerAdvice {
+
+	 @ExceptionHandler(TodoNotFoundException.class)
+	 public ResponseEntity<ErrorResponse> handleTodoNotFound(TodoNotFoundException e)
+	 {
+		 
+		 ErrorResponse er= new ErrorResponse("TODO-404", "Todo with the specified ID Not found!", new Date());
+		  return new ResponseEntity<ErrorResponse>(er,HttpStatus.NOT_FOUND);
+	 }
+}
---
+++ springhelloweb/src/main/java/com/ncs/exception/TodoNotFoundException.java
@@ -0,0 +1,6 @@
+package com.ncs.exception;
+
+
+public class TodoNotFoundException extends RuntimeException{
+
+}
